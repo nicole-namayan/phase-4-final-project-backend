0eb1e6f898670dde82f0acab85d0d07beddf1cd6
Ernest <ernest.wambugu@student.moringaschool.com>
2023-07-05 12:59:58 +0300

    Done with the user_books

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -1,2 +1,11 @@
 class UserBooksController < ApplicationController
+    def create
+        user_book = UserBook.new(user_id: params[:user_id], book_id: params[:book_id])
+        if user_book.save
+            book = Book.find(params[:book_id])
+            render json: book
+        else
+            render json: {error: "Error adding book to user's collection."}
+        end
+    end
 end
