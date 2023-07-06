e6815686efe40293cbcf210be995c8b62064bd87
Ernest <ernest.wambugu@student.moringaschool.com>
2023-07-06 11:43:21 +0300

    Adding a a rating to the table

--- db/migrate/20230706064831_create_books.rb
+++ db/migrate/20230706064831_create_books.rb
@@ -2,6 +2,7 @@ class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.string :imageURL
+      t.integer :rating
       t.string :title
       t.string :description
       t.decimal :price
