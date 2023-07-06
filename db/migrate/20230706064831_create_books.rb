823803b57b920f366f4f3316cf8a75102115127f
Ernest <ernest.wambugu@student.moringaschool.com>
2023-07-06 11:47:52 +0300

    Adding the genre and imageALT tables

--- db/migrate/20230706064831_create_books.rb
+++ db/migrate/20230706064831_create_books.rb
@@ -2,8 +2,10 @@ class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.string :imageURL
+      t.string :imageAlt
       t.integer :rating
       t.string :title
+      t.string :genre
       t.string :description
       t.decimal :price
       t.string :publication_date
