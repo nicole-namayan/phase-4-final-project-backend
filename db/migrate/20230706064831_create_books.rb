e6650628443115ffdc67838a7c5bcd322806a542
nicole <nicolenamayan133@gmail.com>
2023-07-06 10:42:53 +0300

    done with migration

--- db/migrate/20230706064831_create_books.rb
+++ db/migrate/20230706064831_create_books.rb
@@ -1,6 +1,14 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
+      t.string :imageURL
+      t.string :title
+      t.string :description
+      t.decimal :price
+      t.string :publication_date
+      t.string :publisher
+      t.string :author
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
